28aaa4652ac63d7e359bc065b9f804fc356a628f
Rexman17 <a.stone1794@gmail.com>
2019-04-07 19:11:01 -0400

    adding files

---
+++ app/models/Airline.rb
@@ -0,0 +1,47 @@
+# Has many airports through flights
+class Airline
+
+  attr_reader :name, :ceo, :founding_year
+
+  @@all = []
+
+  def initialize(name, ceo, founding_year)
+    @name = name
+    @ceo = ceo
+    @founding_year = founding_year
+
+    @@all << self
+  end
+
+  # INSTANCE METHODS
+  def acquisition(ceo, name)
+    @ceo = ceo
+    @name = name
+  end
+
+  def num_flights
+    my_flights.select do |flight|
+      flight.airline == self
+    end.count
+  end
+
+  # HELPER
+  def my_flights
+    Flight.all.select do |flight|
+      flight.airline == self
+    end
+  end
+
+  # CLASS METHODS
+  def self.all
+    @@all
+  end
+
+  def self.find_by_ceo(ceo)
+    Airline.all.find do |airline|
+      # binding.pry
+      airline.ceo == ceo
+    end
+  end
+
+end
---
+++ app/models/Airport.rb
@@ -0,0 +1,35 @@
+class Airport
+
+  attr_reader :name, :cost_to_build, :dog_friendly
+  @@all = []
+
+  def initialize(name, cost_to_build, dog_friendly)
+    @name = name
+    @cost_to_build = cost_to_build
+    @dog_friendly = dog_friendly
+
+    @@all << self
+  end
+
+  # INSTANCE METHODS
+  def schedule_flight(airline, origin, destination)
+    Flight.new(airline, self, origin, destination)
+  end
+
+  # CLASS METHODS
+  def self.all
+    @@all
+  end
+
+  def self.love_doggos
+    Airport.all.select do |airport|
+      airport.dog_friendly == true
+    end
+  end
+
+  def self.most_expensive
+    Airport.all.max_by { |airport| airport.cost_to_build }
+    # Airport.all.max { |a, b| a.cost_to_build <=> b.cost_to_build }
+  end
+
+end
